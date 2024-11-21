d32146736eb0ba6e1bcca5a3da2bdedb1ffbb9f4
gonzaloiniesta <gon.iniesta@gmail.com>
2024-11-21 19:13:56 +0100

    Successfully retrieved data from database

--- app/mongodb/mongodb.py
+++ app/mongodb/mongodb.py
@@ -4,7 +4,7 @@ import json
 
 
 class MongoDB:
-    def __init__(self, uri="mongodb://localhost:27017", db_name="football_analytics"):
+    def __init__(self, uri="mongodb://admin:adminpassword@localhost:27017/", db_name="football_analytics"):
         """
         Initializes a connection to a MongoDB instance.
 
@@ -57,3 +57,29 @@ class MongoDB:
         matches_collection.insert_many(data["matches"])
 
         print("Data successfully inserted into MongoDB.")
+
+    def get_data(self, collection_name: str, query: dict = None, projection: dict = None) -> list:
+        """
+        Retrieves data from a specified collection in the database.
+
+        Parameters:
+        - collection_name (str): The name of the collection to query.
+        - query (dict): The query filter to apply (default is None, which retrieves all documents).
+        - projection (dict): The fields to include or exclude (default is None, which includes all fields).
+
+        Returns:
+        - list: A list of documents matching the query.
+        """
+        if query is None:
+            query = {}
+
+        collection = self.db[collection_name]
+
+        try:
+            result = list(collection.find(query, projection))
+            print(f"Successfully retrieved data from collection '{collection_name}'.")
+            return result
+        except Exception as e:
+            print(f"An error occurred while retrieving data: {e}")
+            return []
+
--- app/test/test.py
+++ app/test/test.py
@@ -1,24 +1,14 @@
 import json
 import pandas as pd
+from mongodb import MongoDB
 
 # Ruta del archivo JSON
-json_path = "/Users/giniest/FootballAnalytics/footballAnalytics/app/data/football_data.json"
+json_path = "/home/gonzalo/footballAnalytics/app/data/football_data.json"
 
-# Cargar el archivo JSON completo
-with open(json_path, "r") as f:
-    data = json.load(f)
+db = MongoDB()
 
-# Convertir las secciones específicas en DataFrames
-players_df = pd.DataFrame(data["players"])
-teams_df = pd.DataFrame(data["teams"])
-matches_df = pd.DataFrame(data["matches"])
+db.load_data(json_path)
 
-# Mostrar los DataFrames
-print("Players DataFrame:")
-print(players_df["total_goals"])
-
-# print("\nTeams DataFrame:")
-# print(teams_df)
-
-# print("\nMatches DataFrame:")
-# print(matches_df)
+print(db.get_data("players"))
+print("")
+print(db.get_data("league"))
